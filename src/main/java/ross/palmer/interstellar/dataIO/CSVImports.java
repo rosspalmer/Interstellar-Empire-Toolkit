0048e21083ac93e33f9cde3b6be5d4065dfe99f6
Ross Palmer <palmer@opttek.com>
2018-03-31 02:22:42 -0600

    CSV importer for stars into galaxy

---
+++ src/main/java/ross/palmer/interstellar/dataIO/CSVImports.java
@@ -0,0 +1,28 @@
+package ross.palmer.interstellar.dataIO;
+
+import jdk.internal.util.xml.impl.ReaderUTF8;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import ross.palmer.interstellar.simulator.galaxy.Galaxy;
+import ross.palmer.interstellar.simulator.galaxy.StellarSystem;
+
+import java.io.*;
+
+public class CSVImports {
+
+    public static void stars(Galaxy galaxy) throws IOException {
+
+        InputStream stream = new FileInputStream("within300pc.csv");
+        Reader reader = new ReaderUTF8(stream);
+        CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader());
+
+        csvParser.getRecords().forEach(csvRecord -> {
+            StellarSystem stellarSystem = new StellarSystem(csvRecord);
+            galaxy.addStellarSystem(stellarSystem);
+        });
+
+        System.out.println("=== Stellar Systems Generated ===");
+
+    }
+
+}
---
+++ src/test/java/ross/palmer/interstellar/dataIO/CSVImportsTest.java
@@ -0,0 +1,33 @@
+package ross.palmer.interstellar.dataIO;
+
+import org.junit.Test;
+import ross.palmer.interstellar.simulator.galaxy.Galaxy;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class CSVImportsTest {
+
+    @Test
+    public void stars() {
+
+        Galaxy galaxy = new Galaxy();
+        try {
+            CSVImports.stars(galaxy);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(77776, galaxy.getStellarSystems().size());
+        assertEquals(4658227648L, (long) galaxy.getStellarSystems().stream()
+                .mapToDouble(stellarSystem -> stellarSystem.getStarData().getId())
+                .sum());
+
+        assertEquals(133, galaxy.getStellarSystems().stream()
+                .filter(stellarSystem -> stellarSystem.getStarData().getName().isPresent()).count());
+        assertEquals(2846, galaxy.getStellarSystems().stream()
+                .filter(stellarSystem -> stellarSystem.getStarData().getBfDesignation().isPresent()).count());
+
+    }
+}
