993ed8bca1e6848a9a141456d07b4fa4d24057b9
Ross Palmer <palmer@opttek.com>
2018-04-02 23:45:23 -0600

    basic star table gui element

---
+++ src/main/java/ross/palmer/interstellar/gui/StarTableView.java
@@ -0,0 +1,57 @@
+package ross.palmer.interstellar.gui;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import ross.palmer.interstellar.Main;
+import ross.palmer.interstellar.simulator.galaxy.StarData;
+
+
+public class StarTableView extends TableView {
+
+    private ObservableList<StarData> data =
+            FXCollections.observableArrayList();
+
+    public StarTableView() {
+
+        Main.getGalaxy().getStellarSystems()
+                .forEach(stellarSystem -> data.add(stellarSystem.getStarData()));
+
+        TableColumn<StarData, Long> idCol = new TableColumn<>("ID");
+        idCol.setMinWidth(100);
+        idCol.setCellValueFactory(
+                new PropertyValueFactory<>("id"));
+
+        TableColumn<StarData, String> nameCol = new TableColumn<>("Name");
+        nameCol.setMinWidth(100);
+        nameCol.setCellValueFactory(
+                new PropertyValueFactory<>("name"));
+
+        TableColumn<StarData, Double> x = new TableColumn<>("X");
+        x.setMinWidth(10);
+        x.setCellValueFactory(
+                new PropertyValueFactory<>("x"));
+
+        TableColumn<StarData, Double> y = new TableColumn<>("Y");
+        y.setMinWidth(10);
+        y.setCellValueFactory(
+                new PropertyValueFactory<>("y"));
+
+        TableColumn<StarData, Double> z = new TableColumn<>("Z");
+        z.setMinWidth(10);
+        z.setCellValueFactory(
+                new PropertyValueFactory<>("z"));
+
+        TableColumn<StarData, String> spectFull = new TableColumn<>("spectFull");
+        spectFull.setMinWidth(100);
+        spectFull.setCellValueFactory(
+                new PropertyValueFactory<>("spectFull"));
+
+        setItems(data);
+        getColumns().addAll(idCol, nameCol, x, y, z, spectFull);
+
+    }
+
+}
